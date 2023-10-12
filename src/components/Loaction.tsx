e33a52dfb1aafaea7099798de4ff9f0b25f38756
Kiran Singh <kiran.singh@globtier.in>
2023-10-12 22:37:54 +0530

    integrated api

--- src/components/Loaction.tsx
+++ src/components/Loaction.tsx
@@ -1,13 +1,18 @@
-import React from "react";
+import moment from "moment";
 import "../style.css";
-const Loaction = () => {
+
+const Location = () => {
+  const now = moment();
+  const formattedTime = now.format("HH:mm");
+  const formattedDate = now.format("dddd, D MMMM");
+
   return (
     <div className="card location">
       <h2>Noida</h2>
-      <h1>09:03</h1>
-      <p>Thursday, 31 Aug</p>
+      <h1>{formattedTime}</h1>
+      <p>{formattedDate}</p>
     </div>
   );
 };
 
-export default Loaction;
+export default Location;
--- src/components/weatherDetails.tsx
+++ src/components/weatherDetails.tsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../style.css";
 import Sunrise from "../assets/sunrise-white 1.png";
-import Sunset from "../assets/sunset-white 1.png"
-import Clear from "../assets/clear 1.png"
-import Humidity from "../assets/humidity 1.png"
-import Wind from "../assets/wind 1.png"
-import Pressure from "../assets/pressure-white 1.png"
-import Uv from "../assets/uv-white 1.png"
+import Sunset from "../assets/sunset-white 1.png";
+import Clear from "../assets/clear 1.png";
+import Humidity from "../assets/humidity 1.png";
+import Wind from "../assets/wind 1.png";
+import Pressure from "../assets/pressure-white 1.png";
+import Uv from "../assets/uv-white 1.png";
+interface WeatherData {
+  main: {
+    temp: number;
+    feels_like: number;
+    pressure: number;
+    humidity: number;
+  };
+  weather: {
+    main: string;
+  }[];
+  wind: {
+    speed: number;
+  },
+}
 const WeatherDetails = () => {
+  const [detail, setDetail] = useState<WeatherData | null>(null);
+  const apiKey = "628820e4635eda8ed9787634489b3478";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${apiKey}`;
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((res) => res.json())
+      .then((data) => {
+        setDetail(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data: ", error);
+      });
+  }, [apiUrl]);
+
+
   return (
     <div className="card weather-details">
-      <p className="degree">24°C</p>
+      <p className="degree">{detail?.main.temp}°C</p>
       <div className="d-flex">
         <span className="feels-like">Feels like:</span>
-        <span className="feels-like-degree">22°C</span>
+        <span className="feels-like-degree">{detail?.main.feels_like}°C</span>
       </div>
       <div className="d-flex Sunrise">
         <img className="img-sunrise" src={Sunrise} alt="" />
@@ -28,29 +58,29 @@ const WeatherDetails = () => {
           <span className="sunset-name">Sunset</span>
           <span className="sunset-time">20:37 AM</span>
         </div>
-      </div> 
-      <div >
-      <img className="clear"src={Clear} alt="" />
-      <p className="clear-name">Sunny</p>
+      </div>
+      <div>
+        <img className="clear" src={Clear} alt="" />
+        <p className="clear-name">{detail?.weather[0].main}</p>
       </div>
       <div className="d-flex flex-column humidity">
         <img className="humidity-img" src={Humidity} alt="" />
-        <span className="humidity-value">41%</span>
+        <span className="humidity-value">{detail?.main.humidity}</span>
         <span className="humidity-name">Humidity</span>
       </div>
       <div className="d-flex flex-column wind">
-      <img className="wind-img" src={Wind} alt="" />
-        <span className="wind-value">2km/h</span>
+        <img className="wind-img" src={Wind} alt="" />
+        <span className="wind-value">{detail?.wind.speed}</span>
         <span className="wind-name">Wind Speed</span>
       </div>
       <div className="d-flex flex-column pressure">
-      <img className="pressure-img" src={Pressure} alt="" />
-        <span className="pressure-value">997hP</span>
+        <img className="pressure-img" src={Pressure} alt="" />
+        <span className="pressure-value">{detail?.main.pressure}</span>
         <span className="pressure-name">Pressure</span>
       </div>
       <div className="d-flex flex-column uv">
-      <img className="uv-img" src={Uv} alt="" />
-        <span className="uv-value" >8</span>
+        <img className="uv-img" src={Uv} alt="" />
+        <span className="uv-value">8</span>
         <span className="uv-name">UV</span>
       </div>
     </div>
